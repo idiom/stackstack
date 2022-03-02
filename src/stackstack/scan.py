68f5ccc7a37b00e72d32941a51c513e580f2a9e7
Alberto Ortega <a0rtega@users.noreply.github.com>
2022-03-02 17:45:06 +0100

    Change scanning logic to iterate matches in the same order always. Minor change in scan_function to avoid skipping the beginning of functions.

--- src/stackstack/scan.py
+++ src/stackstack/scan.py
@@ -93,8 +93,8 @@ class YaraScanner(ScanEngineBase):
             func_name = idc.get_func_name(func_entry)
 
             func_data = idc.get_bytes(func_entry, idc.get_func_attr(func_entry, idc.FUNCATTR_END) - func_entry)
+            match_strings = []
             for rule in self.rules:
-                last_match_offset = 0
                 matches = rule.match(data=func_data)
 
                 if not matches:
@@ -102,18 +102,21 @@ class YaraScanner(ScanEngineBase):
 
                 for rule_match in matches:
                     for match in rule_match.strings:
+                        match_strings.append(match)
 
-                        if last_match_offset > match[0] > last_match_offset - match_overlay_range:
-                            continue
-                        elif last_match_offset < match[0] < last_match_offset + match_overlay_range:
-                            continue
+            last_match_offset = 0
+            for match in sorted(match_strings, key=lambda d: d[0]): # order matches by ascending offset
+                if last_match_offset > match[0] > last_match_offset - match_overlay_range:
+                    continue
+                elif last_match_offset < match[0] < last_match_offset + match_overlay_range:
+                    continue
 
-                        self.logger.debug("Match at [%x]" % (func_entry + match[0]))
-                        last_match_offset = match[0]
-                        try:
-                            results[func_name] += 1
-                        except KeyError:
-                            results[func_name] = 1
+                self.logger.debug("Match at [%x]" % (func_entry + match[0]))
+                last_match_offset = match[0]
+                try:
+                    results[func_name] += 1
+                except KeyError:
+                    results[func_name] = 1
         return results
 
     def scan_function(self, data, match_overlay_range=64):
@@ -124,9 +127,9 @@ class YaraScanner(ScanEngineBase):
         :return:
         """
         values = []
+        match_strings = []
         self.logger.error("Scanning function")
         for rule in self.rules:
-            last_match_offset = 0
             matches = rule.match(data=data)
 
             if not matches:
@@ -134,13 +137,19 @@ class YaraScanner(ScanEngineBase):
 
             for rule_match in matches:
                 for match in rule_match.strings:
-                    if last_match_offset > match[0] > last_match_offset - match_overlay_range:
-                        continue
-                    elif last_match_offset < match[0] < last_match_offset + match_overlay_range:
-                        continue
+                    match_strings.append(match)
 
-                    self.logger.debug("Match at %x" % match[0])
-                    last_match_offset = match[0]
-                    values.append(match[0])
+        # Assign last_match_offset with the negative value of match_overlay_range
+        # to avoid skipping matches at the beginning of the function
+        last_match_offset = -abs(match_overlay_range)
+        for match in sorted(match_strings, key=lambda d: d[0]): # order matches by ascending offset
+            if last_match_offset > match[0] > last_match_offset - match_overlay_range:
+                continue
+            elif last_match_offset < match[0] < last_match_offset + match_overlay_range:
+                continue
+
+            self.logger.debug("Match at %x" % match[0])
+            last_match_offset = match[0]
+            values.append(match[0])
         return values
 
